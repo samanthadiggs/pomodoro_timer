5c8f81e16751fd437fee5b3d48e65e897a03f25d
samantha diggs <99908108+samanthadiggs@users.noreply.github.com>
2023-03-07 18:52:08 -0500

    finishing the js but reset and hide timer.........

--- script.js
+++ script.js
@@ -21,6 +21,36 @@ function start() {
         document.getElementById('seconds').innerHTML = seconds;
 
         seconds = seconds - 1;
+        if(seconds == 0) {
+            workMinutes = workMinutes - 1;
+            if (workMinutes === -1){
+                if(breakCount % 2 === 0){
+                    workminutes = breakMinutes;
+                    breakCount++
+                    workTitle.classList.remove('active')
+                    breakTitle.classList.add('active')
+                } else {
+                    workMinutes = workTime;
+                    breakCount ++
+                    breakTitle.classList.remove('active');
+                    workTitle.classList.add('active')
+                }
+            }
+            seconds = 59;
+        }
     }
-    setInterval(timerFunction, 1000);
+    setInterval(timerFunction, 1000); // 1000 = 1 second
+}
+
+function reset(){
+    let workTime = 45;
+    let breakTime = 10;
+    let seconds = '00'
+    breakCount = 0;
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+}
+
+function hideTimer(){
+    document.getElementByClassName('timer').style.display = none;
 }
